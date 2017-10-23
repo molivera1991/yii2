3ea5833d247de0c09696aa4ebade3835eaa4080b
Sofia2512 <rodriguezsofia2512@gmail.com>
2017-10-23 15:14:58 -0300

    Update m171021_222747_create_user_table.php
    Corrección en la migración create user table

--- console/migrations/m171021_222747_create_user_table.php
+++ console/migrations/m171021_222747_create_user_table.php
@@ -13,7 +13,19 @@ class m171021_222747_create_user_table extends Migration
     public function up()
     {
         $this->createTable('user', [
-            'id' => $this->primaryKey(),
+              `id` int(11) NOT NULL AUTO_INCREMENT,
+              `name` varchar(45) NOT NULL,
+              `last_name` varchar(45) NOT NULL,
+              `ci` char(8) NOT NULL,
+              `username` varchar(255) NOT NULL,
+              `auth_key` varchar(32) NOT NULL,
+              `password_hash` varchar(255) NOT NULL,
+              `password_reset_token` varchar(255) NOT NULL,
+              `email` varchar(100) NOT NULL,
+              `status` smallint(10) NOT NULL,
+              `created_at` int(11) NOT NULL,
+              `updated_at` int(11) NOT NULL,
+              PRIMARY KEY (`id`)
         ]);
     }
 
